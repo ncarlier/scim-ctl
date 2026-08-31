235c70d3ef82417328067ebcdb00885bd8194aca
Nicolas Carlier <n.carlier@nunux.org>
2026-08-31 17:25:15 +0200

    feat: print import progression

--- internal/commands/import.go
+++ internal/commands/import.go
@@ -50,8 +50,19 @@ Examples:
 
 		// Setup input stream
 		var reader io.Reader
+		totalLines := 0
+
 		if importFile != "" {
+			// Count total lines for progress tracking
 			file, err := os.Open(importFile)
+			if err != nil {
+				return fmt.Errorf("failed to open input file: %w", err)
+			}
+			totalLines = countLines(file)
+			file.Close()
+
+			// Reopen for actual reading
+			file, err = os.Open(importFile)
 			if err != nil {
 				return fmt.Errorf("failed to open input file: %w", err)
 			}
@@ -75,11 +86,20 @@ Examples:
 		buf := make([]byte, maxCapacity)
 		scanner.Buffer(buf, maxCapacity)
 
+		stderrStat, _ := os.Stderr.Stat()
+		isStderrTerminal := (stderrStat.Mode() & os.ModeCharDevice) != 0
+
 		var batch []scim.BulkOperation
 		var totalSuccess, totalErrors int
+		processedCount := 0
+		lastReportedPercent := -1
 		lineNum := 0
 
 		resourcePath := "/" + scim.ResourceName(importResourceType)
+
+		if isStderrTerminal && totalLines > 0 {
+			fmt.Fprintf(os.Stderr, "Total resources to import: %d\n", totalLines)
+		}
 
 		flushBatch := func() error {
 			if len(batch) == 0 {
@@ -104,7 +124,7 @@ Examples:
 				// Print the result
 				jsonData, err := json.Marshal(opResp)
 				if err != nil {
-					fmt.Fprintln(os.Stderr, "Failed to marshal response:", err)
+					fmt.Fprintln(os.Stderr, "\nFailed to marshal response:", err)
 				} else {
 					fmt.Println(string(jsonData))
 				}
@@ -124,7 +144,7 @@ Examples:
 
 			var resourceData scim.Resource
 			if err := json.Unmarshal([]byte(line), &resourceData); err != nil {
-				fmt.Fprintf(os.Stderr, "Error parsing JSON at line %d: %v\n", lineNum, err)
+				fmt.Fprintf(os.Stderr, "\nError parsing JSON at line %d: %v\n", lineNum, err)
 				totalErrors++
 				continue
 			}
@@ -142,6 +162,20 @@ Examples:
 				if err := flushBatch(); err != nil {
 					return err
 				}
+				
+				processedCount += importChunkSize
+				
+				if isStderrTerminal {
+					if totalLines > 0 {
+						currentPercent := (processedCount * 100) / totalLines
+						if currentPercent > lastReportedPercent {
+							fmt.Fprintf(os.Stderr, "Importing... %d%%\r", currentPercent)
+							lastReportedPercent = currentPercent
+						}
+					} else {
+						fmt.Fprintf(os.Stderr, "Importing... %d processed\r", processedCount)
+					}
+				}
 			}
 		}
 
@@ -150,8 +184,18 @@ Examples:
 		}
 
 		// Flush remaining
+		remainingCount := len(batch)
 		if err := flushBatch(); err != nil {
 			return err
+		}
+		
+		processedCount += remainingCount
+		if isStderrTerminal {
+			if totalLines > 0 {
+				fmt.Fprintf(os.Stderr, "Importing... 100%%\n")
+			} else {
+				fmt.Fprintf(os.Stderr, "Importing... %d processed\n", processedCount)
+			}
 		}
 
 		report := map[string]interface{}{
@@ -173,6 +217,8 @@ Examples:
 		return nil
 	},
 }
+
+// Note: countLines function will be shared from bulk_update.go in the same package
 
 func init() {
 	rootCmd.AddCommand(importCmd)
